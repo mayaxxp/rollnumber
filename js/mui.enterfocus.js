37e457c406eb67c9dbdac143fa5dba704f1d07dc
mayaxxp <mayaxxp@sina.com>
2019-08-26 17:49:03 +0800

    车辆顶视状态图

--- js/mui.enterfocus.js
+++ js/mui.enterfocus.js
@@ -9,17 +9,58 @@
 					if (event.keyCode == 13){
 						var boxIndex = boxArray.indexOf(this);
 						if (boxIndex == boxArray.length - 1) {
-							if (callback) callback();
+//							if (callback) callback();
 						} else {
 							boxIndex=boxIndex+1
 							var nextBox = boxArray[boxIndex];
 							jQ(nextBox).focus();
 						}
 					}else{
-						if (callback) callback("有回调吗？我要调");
+//						if (callback) callback("有回调吗？我要调");
 					}
 				}, false);
+				jQ(box).bind("input",function(event){
+					var textold=jQ(this).val();
+//					console.log(textold)
+					if(textold==""){
+						jQ(".mui-input-row").each(function(i,item){ 
+							jQ(item).removeClass("currRow");
+						}) 
+						return;
+					}
+					jQ(".mui-input-row").each(function(i,item){
+						jQ(".currRow>.numcontrol").find(".glyphicon-plus").unbind("click");
+						jQ(".currRow>.numcontrol").find(".glyphicon-minus").unbind("click"); 
+						jQ(item).removeClass("currRow");
+					}) 
+					jQ(this).parent(".mui-input-row").addClass("currRow"); 
+					var _rootThis=this;
+					var boxIndex = boxArray.indexOf(this);
+					 
+					if(jQ(this).parent(".mui-input-row").find(".numcontrol").length==0){
+						jQ(this).after('<span class="numcontrol"><span class="glyphicon glyphicon-minus"></span><span class="glyphicon glyphicon-plus"></span></span>'); 
+					}
+					var offsettop=this.parentNode.offsetTop+this.parentNode.parentNode.offsetTop-50;
+					jQ("#infocontent").scrollTop(offsettop);
+					jQ(this).next(".numcontrol").find(".glyphicon-plus").bind("click",function(){  
+//							var t_=Number(jQ(_rootThis).val())+0.5;
+//							jQ(_rootThis).val(t_)
+						if (callback) callback(true);
+							console.log("当前值+0.5"); 
+					});
+					jQ(this).next(".numcontrol").find(".glyphicon-minus").bind("click",function(){  
+//						var t_=Number(jQ(_rootThis).val())-0.5;
+//							jQ(_rootThis).val(t_)
+						if (callback) callback(false);
+						console.log("当前值-0.5");
+					});
+				})
 				jQ(box).bind("focus",function(event){ 
+					var textold=jQ(this).val();
+//						console.log(textold)
+						if(textold==""){
+							return;
+						}
 						jQ(".mui-input-row").each(function(i,item){
 							jQ(".currRow>.numcontrol").find(".glyphicon-plus").unbind("click");
 							jQ(".currRow>.numcontrol").find(".glyphicon-minus").unbind("click"); 
@@ -35,12 +76,11 @@
 						var offsettop=this.parentNode.offsetTop+this.parentNode.parentNode.offsetTop-50;
 						jQ("#infocontent").scrollTop(offsettop);
 						jQ(this).next(".numcontrol").find(".glyphicon-plus").bind("click",function(){  
- 							jQ(_rootThis).value+=.5;
+							if (callback) callback(true);
 							console.log("当前值+0.5");
-							
 						});
 						jQ(this).next(".numcontrol").find(".glyphicon-minus").bind("click",function(){  
-							jQ(_rootThis).value-=.5;
+							if (callback) callback(false);
 							console.log("当前值-0.5");
 						});
 				}) ; 
