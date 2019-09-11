79553cbb5811b4fad3a850d1ffbcde7fbaf53b05
mayaxxp <mayaxxp@sina.com>
2019-09-11 18:09:14 +0800

    qq

--- emcsprototype/src/components/MainPageTop.vue
+++ emcsprototype/src/components/MainPageTop.vue
@@ -6,131 +6,15 @@
 					<div class="bd mt20">
 						<div id="sca1" class="warp-pic-list">
 							<div id="wrapBox1" class="wrapBox">
-							 
-								<ul id="count1" class="count clearfix"  >
-							
-										<li>
-											<a class="teg" id="GLBJCDD" href="javascript:void(0)" @click="backHome">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/e5.png"></div>
-												<div class="wef">{{menulist}}道路清洁车调度</div>
+							 	<ul id="count1" class="count clearfix"  >
+										<li v-for="{id,cnName,imgUrl,beselected} in dataMenus">
+											<a  :class="{'teg':beselected}"  :id="id" href="javascript:void(0)" @click="getLeftMenu(id)">
+												<div class="kwg"><img :src="imgUrl"></div>
+												<div class="wef">{{cnName}}</div>
 											</a>
-										</li>
-							
-										<li>
-											<a class="" id="CCDD" href="javascript:void(0)" @click="getLeftMenu('CCDD','CCDD','垃圾车调度','/dispatch/toMain.do')">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/ljcdd.png"></div>
-												<div class="wef">垃圾车调度</div>
-											</a>
-										</li>
-							
-										<li>
-											<a class="" id="FBCDD" href="javascript:void(0)" @click="getLeftMenu('FBCDD','FBCDD','粪便车调度','/fb/toMain.do')">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/fbcdd.png"></div>
-												<div class="wef">粪便车调度</div>
-											</a>
-										</li>
-							
-										<li>
-											<a class="" id="JCCDD" href="javascript:void(0)" @click="getLeftMenu('JCCDD','JCCDD','检查车调度','/jc/toMain.do')">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/jccdd.png"></div>
-												<div class="wef">检查车调度</div>
-											</a>
-										</li>
-							
-										<li>
-											<a class="" id="GCBJ" href="javascript:void(0)" @click="getLeftMenu('GCBJ','GCBJ','公厕保洁','')">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/e3.png"></div>
-												<div class="wef">公厕保洁</div>
-											</a>
-										</li>
-							
-										<li>
-											<a class="" id="ZYML" href="javascript:void(0)" @click="getLeftMenu('ZYML','ZYML','资源目录','')">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/menu/18resource/resource.png"></div>
-												<div class="wef">资源目录</div>
-											</a>
-										</li>
-							
-										<li>
-											<a class="" id="JZLJ" href="javascript:void(0)" @click="getLeftMenu('JZLJ','JZLJ','建筑垃圾','')">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/e4.png"></div>
-												<div class="wef">建筑垃圾</div>
-											</a>
-										</li>
-							
-										<li>
-											<a class="" id="RCGZ" href="javascript:void(0)" @click="getLeftMenu('RCGZ','RCGZ','日常工作','')">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/e2.png"></div>
-												<div class="wef">日常工作</div>
-											</a>
-										</li>
-							
-										<li>
-											<a class="" id="CLBJ" href="javascript:void(0)" @click="getLeftMenu('CLBJ','CLBJ','车辆报警','')">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/ebj.png"></div>
-												<div class="wef">车辆报警</div>
-											</a>
-										</li>
-							
-										<li>
-											<a class="" id="SPJG" href="javascript:void(0)" @click="getLeftMenu('SPJG','SPJG','视频监管','')">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/e6.png"></div>
-												<div class="wef">视频监管</div>
-											</a>
-										</li>
-							
-										<li>
-											<a class="" id="YJZH" href="javascript:void(0)" @click="getLeftMenu('YJZH','YJZH','应急指挥','')">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/e9.png"></div>
-												<div class="wef">应急指挥</div>
-											</a>
-										</li>
-							
-										<li>
-											<a class="" id="ZDCL" href="javascript:void(0)" @click="getLeftMenu('ZDCL','ZDCL','终端处置','')">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/e7.png"></div>
-												<div class="wef">终端处置</div>
-											</a>
-										</li>
-							
-										<li>
-											<a class="" id="ZYKH" href="javascript:void(0)" @click="getLeftMenu('ZYKH','ZYKH','作业考核','')">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/e8.png"></div>
-												<div class="wef">作业考核</div>
-											</a>
-										</li>
-							
-										<li>
-											<a class="" id="ZHTJ" href="javascript:void(0)" @click="getLeftMenu('ZHTJ','ZHTJ','综合统计','')">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/e10.png"></div>
-												<div class="wef">综合统计</div>
-											</a>
-										</li>
-							
-										<li>
-											<a class="" id="MHGL" href="javascript:void(0)" @click="getLeftMenu('MHGL','MHGL','门户管理','')">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/e1.png"></div>
-												<div class="wef">门户管理</div>
-											</a>
-										</li>
-							
-										<li>
-											<a class="" id="TCGL" href="javascript:void(0)" @click="getLeftMenu('TCGL','TCGL','图层管理','')">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/tc_mgr.png"></div>
-												<div class="wef">图层管理</div>
-											</a>
-										</li>
-							
-										<li>
-											<a class="" id="SYS" href="javascript:void(0)" @click="getLeftMenu('SYS','SYS','系统管理','/queryUserMain.do')">
-												<div class="kwg"><img src="../assets/images/mainTopbanner/sys.png"></div>
-												<div class="wef">系统管理</div>
-											</a>
-										</li>
-							
-									</ul>
-							</div>
-						 	  
+										</li> 
+								</ul>
+							</div> 
 							<a id="right1" class="prev icon btn"></a>
 							<a id="left1" class="next icon btn"></a>
 							 
@@ -149,36 +33,54 @@
 <script>
 	export default {
 		name: 'MainPageTop',
+		props: {
+			dataMenus:{
+		      type: Array,
+		      default: [{
+						id:"id",
+						cnName:"无菜单",
+						imgUrl:require("../assets/images/mainTopbanner/e5.png"),
+						beselected:true
+					}]
+		    }
+			
+		},
 		data:()=>{
 			return {
-				menulist:this.dataMenus
+				menulist:[]
 			}
 		},
-		props: {
-			dataMenus:Array
+		created () {  //加载完成之前，执行。执行顺序:父组件-子组件
+		   this.menulist=this.dataMenus;
+		   console.log("【menulist】："+this.menulist); // 空值
+		   console.log(this.dataMenus);
+	    },
+		mounted(){ //页面初始化方法   加载完成后执行。执行顺序:子组件-父组件
+			
 		},
-		methods: {
+		methods: {  //监听方法click事件等   事件方法执行。
 			backHome:() => {
-				console.log(this.menulist); 
+				 if(this.dataMenus){
+				 	console.log(this.dataMenus);
+				 }
 			},
-			getLeftMenu: () => {}
+			getLeftMenu:(ID) => {
+				console.log(event)
+				var thisMene= this.menulist.map((menu)=>{
+					return menu.id=ID
+				})
+				console.log(thisMene)
+				thisMene.beselected=true;
+				
+			}
+		},
+		watch:{  //去监听一个值的变化，然后执行相对应的函数
+			
 		}
 	}
 </script>
 
 <style>
-	/*.center_contentbox{height:719px;top: 80px;}
-	.bottomline{height: 26px;line-height: 30px;background:#0e4980 !important;}
-	.top_banner_mid{height: 80px;overflow-y: hidden;}
-	.panel_Top{height:24px;}
-	.panel_content{height:90%;height:calc( 100% - 24px);height: -webkit-calc(100% - 24px);height: -moz-calc(100% - 24px); } 
-	.panel_Top .panel_TL{height:24px;}
-	.panel_Top .panel_TC span{height:24px;background-position-y:2px;}
-	.panel_Top .panel_TC{height:24px;line-height: 24px;}
-	.panel_Top .panel_TR{height:24px;}
-	.panel_Top .panel_TR .b_toggleMax{margin-top: 5px;}
-	.panel_Top .panel_TR .b_toggleMin{margin-top: 5px;}
-	.panel_Top .panel_TR .r_toggleMin{margin-top: 5px;}   */
 	.top_banner{margin: 0;
 			    display: block;
 			    width: 100%;
@@ -210,7 +112,10 @@
 	    padding: 0px !important;
 	    line-height: 15px;
 	    text-align: center;
-	    width:100%;height:auto;color: #FFF;font-size: 13px;  
+	    width:100%;
+	    height:auto;
+	    color: #FFF;
+	    font-size: 13px;  
 	}
 	.toHome a{display: block;width:56px;height:56px;float:right;margin-right:20px;}
 	.toHome a i{background: url(../assets/images/mainTopbanner/toHomeN.png) 0px 0px no-repeat;pointer-events:none;width:56px;height:56px;display:block;position:relative;}
@@ -218,21 +123,76 @@
 	.toHome a:active i{background: url(../assets/images/mainTopbanner/toHomeD.png) 0px 0px no-repeat;cursor:hand;}
 	.kuaij{width:100px;}
 	
-	.count{width: 528px; height: 80px;display: inline;width: 2000px;  
-    		overflow: hidden; }
-	.count li{display: inline;float: left; list-style: none;margin-right: 4px;}
-	.logo {
-	    background-image: url(../assets/images/mainTopbanner/logo.jpg);
-	    position: absolute;
-	    height: 94px;
-	    width: 430px;
-	    left: 0px;
-	    top: 0px;
-	    background-repeat: no-repeat;
-	    background-position: 0px 0px;
-	    z-index: 11;
+.rowE .count{width:auto;height: 100%;padding: 0px;overflow:hidden;white-space: nowrap;display: inline-block;}
+.rowE .count  a {
+	display: block;
+	width: 75px;
+	height: 86px;
+	text-align: center;
+	font-weight: normal; 
+	line-height: 36px;
+	font-size: 13px;
+	letter-spacing: .1em;
+	text-decoration: none;
+	color: #FFF;
+	padding-top: 10px;
+}
+.kwg img {
+	padding: 0px;
+	float: left;
+	border-top-width: 0px;
+	border-right-width: 0px;
+	border-bottom-width: 0px;
+	border-left-width: 0px;
+	border-top-style: none;
+	border-right-style: none;
+	border-bottom-style: none;
+	border-left-style: none;
+	margin-top: 0px;
+	margin-right: 11px;
+	margin-bottom: 0px;
+	margin-left: 11px;
+}
+.rowE .count a:hover{
+	background-image: url(../assets/images/mainTopbanner/w13O.png);
+	background-repeat: no-repeat;
+    background-position: 0px 0px;
 	}
-	.navBar {
+
+.rowE .count .teg {
+	background-image: url(../assets/images/mainTopbanner/w13.png);
+	background-repeat: no-repeat;
+	background-position: 0px 0px;
+}
+
+.rowE .count .teg:hover {
+	background-image: url(../assets/images/mainTopbanner/w13O.png);
+}
+.rowE .count .teg:ACTIVE {
+	background-image: url(../assets/images/mainTopbanner/w13.png);
+}
+.rowE .count  .kwg {
+}
+.wef {
+	margin: 0px !important;
+	padding: 0px !important;
+	line-height: 15px;
+	text-align: center;
+	width: 68px;
+}
+.count li{display: inline-block;  list-style: none;margin-right: 4px;}
+.logo {
+    background-image: url(../assets/images/mainTopbanner/logo.jpg);
+    position: absolute;
+    height: 94px;
+    width: 430px;
+    left: 0px;
+    top: 0px;
+    background-repeat: no-repeat;
+    background-position: 0px 0px;
+    z-index: 11;
+}
+.navBar {
 	width: 810px;
 	height: 80px;
 	line-height: 37px;
@@ -254,9 +214,6 @@
 	padding-right: 35px;
 	padding-left: 35px;
 }
- 
- 
-
 
 .rowE .btn{
 	display: block;
@@ -267,7 +224,6 @@
 	z-index: 200;
 	cursor: pointer;
 }
-
 .rowE .prev{
 	background-position: -0px -70px;
 	left: 5px;
@@ -289,21 +245,7 @@
 	background:url("../assets/images/mainTopbanner/rowEbtnD.png") left bottom;
 }
 
-.rowE .count{width:auto;height: 100%;overflow:hidden;display: inline-block;}
-.rowE .count  a {
-	display: inline;
-	width: 75px;
-	text-align: center;
-	font-weight: normal;
-	float: left;
-	height: 86px;
-	line-height: 36px;
-	font-size: 13px;
-	letter-spacing: .1em;
-	text-decoration: none;
-	color: #FFF;
-	padding-top: 10px;
-}
+
 
 
 
@@ -363,4 +305,40 @@
     position: relative;}
     .kuaij .toHome .routelink:hover i{background: url(../assets/images/mainTopbanner/toHomeO.png) 0px 0px no-repeat;}
 	.kuaij .toHome .routelink:active i{background: url(../assets/images/mainTopbanner/toHomeN.png) 0px 0px no-repeat;}
+	
+	
+	.icon {
+	    background-image: url(../assets/images/mainTopbanner/icon.png);
+	    background-repeat: no-repeat;
+	    background-position: 0 0;
+	}
+	.rowE .btn {
+	    display: block;
+	    height: 45px;
+	    position: absolute;
+	    top: 22px;
+	    width: 28px;
+	    z-index: 200;
+	    cursor: pointer;
+	}
+	.rowE .prev:hover {
+	    background-position: -0px -115px;
+	}
+	.rowE .prev {
+	    background-position: -0px -70px;
+	    left: 5px;
+	}
+	.rowE .prev:ACTIVE{
+		background-position: left top;
+	}
+	.rowE .next:hover {
+	    background-position: -0px -205px;
+	}
+	.rowE .next {
+	    background-position: -0px -160px;
+	    right: -39px;
+	}
+	.rowE .next:ACTIVE{
+		background-position:left bottom;
+	}
 </style>
--- emcsprototype/src/views/Secendpage.vue
+++ emcsprototype/src/views/Secendpage.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="secendpage">
-    <MainPageTop data-menus=[1,11,23,13,14]></MainPageTop>
+    <MainPageTop :data-menus="childlist"></MainPageTop>
     
   </div>
 </template>
@@ -8,11 +8,117 @@
 <script>
 	import MainPageTop from '../components/MainPageTop'
 //	import '../assets/css/mainTopbanner/index_sec.css'
-// @ is an alias to /src 
+// ../ is an alias to /src 
 export default {
   name: 'Firstpage',
   components: {MainPageTop},
-  poprs:String
+//poprs:Array,
+  data:()=>{
+  	return {
+  		childlist:[{
+											id:"GLBJCDD",
+											cnName:"道路清洁车调度",
+											imgUrl:require("../assets/images/mainTopbanner/e5.png"),
+											beselected:true
+										},
+										{
+											id:"CCDD",
+											cnName:"垃圾车调度",
+											imgUrl:require("../assets/images/mainTopbanner/ljcdd.png"),
+											beselected:false
+										},
+										{
+											id:"FBCDD",
+											cnName:"粪便车调度",
+											imgUrl:require("../assets/images/mainTopbanner/fbcdd.png"),
+											beselected:false
+										},
+										{
+											id:"JCCDD",
+											cnName:"检查车调度",
+											imgUrl:require("../assets/images/mainTopbanner/jccdd.png"),
+											beselected:false
+										},
+										{
+											id:"GCBJ",
+											cnName:"公厕保洁",
+											imgUrl:require("../assets/images/mainTopbanner/e3.png"),
+											beselected:false
+										},
+										{
+											id:"ZYML",
+											cnName:"资源目录",
+											imgUrl:require("../assets/images/mainTopbanner/menu/18resource/resource.png"),
+											beselected:false
+										},
+										{
+											id:"JZLJ",
+											cnName:"建筑垃圾",
+											imgUrl:require("../assets/images/mainTopbanner/e4.png"),
+											beselected:false
+										},
+										{
+											id:"RCGZ",
+											cnName:"日常工作",
+											imgUrl:require("../assets/images/mainTopbanner/e2.png"),
+											beselected:false
+										},
+										{
+											id:"CLBJ",
+											cnName:"车辆报警",
+											imgUrl:require("../assets/images/mainTopbanner/ebj.png"),
+											beselected:false
+										},
+										{
+											id:"SPJG",
+											cnName:"视频监管",
+											imgUrl:require("../assets/images/mainTopbanner/e6.png"),
+											beselected:false
+										},
+										{
+											id:"YJZH",
+											cnName:"应急指挥",
+											imgUrl:require("../assets/images/mainTopbanner/e9.png"),
+											beselected:false
+										},
+										{
+											id:"ZDCZ",
+											cnName:"终端处置",
+											imgUrl:require("../assets/images/mainTopbanner/e7.png"),
+											beselected:false
+										},
+										{
+											id:"ZYKH",
+											cnName:"作业考核",
+											imgUrl:require("../assets/images/mainTopbanner/e8.png"),
+											beselected:false
+										},
+										{
+											id:"ZHTJ",
+											cnName:"综合统计",
+											imgUrl:require("../assets/images/mainTopbanner/e10.png"),
+											beselected:false
+										},
+										{
+											id:"MHGL",
+											cnName:"门户管理",
+											imgUrl:require("../assets/images/mainTopbanner/e1.png"),
+											beselected:false
+										},
+										{
+											id:"TCGL",
+											cnName:"图层管理",
+											imgUrl:require("../assets/images/mainTopbanner/tc_mgr.png"),
+											beselected:false
+										},
+										{
+											id:"SYS",
+											cnName:"系统管理",
+											imgUrl:require("../assets/images/mainTopbanner/sys.png"),
+											beselected:false
+										}]
+  	}
+  }
 }
 </script>
 <style  >
