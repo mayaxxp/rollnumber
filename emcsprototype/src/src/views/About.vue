bf256df55c46e968a6f6b33f1b4dcfc2870253ca
mayaxxp <mayaxxp@sina.com>
2019-09-16 18:20:22 +0800

    侧边栏ok

---
+++ emcsprototype/src/src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script >
+	 
+export default {
+  name: 'About',
+  components: { 
+  },
+  methods:{
+  	loginHander:()=>{ 
+  		window.location.href='/first'
+  	}
+  }
+}
+</script>
+<style>
+  #about {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #FFFFFF;
+    background: red;
+    width: 100%;
+    height: 100%;
+  }
+</style>
---
+++ emcsprototype/src/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="homepage"> 
+      <router-view></router-view>  
+  </div> 
+</template>
+
+<script> 
+import Firstpage from './Firstpage'
+  export default {
+    name: 'Home',
+    components: { 
+      Firstpage
+    }
+  }
+    
+</script>
+<style>
+#homepage{
+ 
+  background:darkgreen;
+  color: #FFFFFF;
+  width: 100%;
+  height: 100%;
+}
+</style>
